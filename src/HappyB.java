4560e16d78784d28803329ee64162a9e1eb4713a
vladabazel <vladabazel@gmail.com>
2020-08-01 10:50:22 -0500

    update HappyB.java

--- src/HappyB.java
+++ src/HappyB.java
@@ -3,5 +3,6 @@ public class HappyB {
         System.out.println("Happy B20!");
         System.out.println("Be Happy!");
         System.out.println("Hello World");
+        System.out.println("   ");
     }
 }
