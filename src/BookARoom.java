ad998e17957c61d17a1956f1c7a022918dfed23c
vladabazel <68966017+vladabazel@users.noreply.github.com>
2020-08-01 15:35:47 -0500

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -1,7 +1,8 @@
 public class BookARoom {
     public static void main(String[] args) {
         System.out.println("Lets be great!");
-        System.out.println("Sometimes you win, sometimes you learn!");
+        System.out.println("Sometimes you win, sometimes you learn! Yes yes");
+        
         
     }
 }
