e45a5c3d9cab7a1cfe503767761bc94fc478efc6
vladabazel <vladabazel@gmail.com>
2020-08-01 10:46:41 -0500

    add HappyB.java

--- src/HappyB.java
+++ src/HappyB.java
@@ -2,5 +2,6 @@ public class HappyB {
     public static void main(String[] args) {
         System.out.println("Happy B20!");
         System.out.println("Be Happy!");
+        System.out.println("Hello World");
     }
 }
